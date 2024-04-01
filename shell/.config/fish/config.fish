912f5fbeb344d794a006144ba77e9db14f0beec9
Sven Palberg <sven@palberg.de>
2024-04-01 18:43:28 +0200

    finetuning

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -1,10 +1,23 @@
-set fish_greeting # disable greeting message
+set fish_greeting
+
+alias cp 'rsync --info=progress2'
+alias vim nvim
+alias cat bat
+
+set -x TERM xterm-256color
+set -x EDITOR vim
+set -x VISUAL vim
 
 abbr -a c clear
 abbr -a d docker
 abbr -a dc docker compose
 abbr -a tf terraform
 abbr -a k kubectl
+abbr -a h helm
+abbr -a g git
+abbr -a df 'dfc -moTs'
+abbr -a du 'du -hHLl'
+abbr -a fd 'fd -HI'
 
 if status is-interactive
     if type -q zoxide
